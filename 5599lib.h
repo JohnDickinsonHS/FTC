9221b43790eb4119fca641ba7870a4fd79d37e47
Ezra Brooks <ezra.m.brooks@gmail.com>
2014-01-29 14:16:10 -0500

    dropped unnecessary variables
    also fixed pragma, rewrote autonomous using lib functions, and some
    reorganization

--- 5599lib.h
+++ 5599lib.h
@@ -1,13 +1,17 @@
 #ifndef TEAM_LIBRARY
 #define TEAM_LIBRARY
+//standard variables
 
-#pragma config(Hubs,  S1, HTMotor,  none,     none,     none)
-#pragma config(Hubs,  S2, HTMotor,  none,     none,     none)
-#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Sensor, S2,     ,               sensorI2CMuxController)
-#pragma config(Motor,  mtr_S1_C1_1,     motorD,        tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C1_2,     motorE,        tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S2_C1_1,     motorF,        tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S2_C1_2,     motorG,        tmotorTetrix, openLoop)
-
+//standard functions
+void runDriveMotors(int power, int msecs){
+	motor[leftMotor] = power;
+	motor[rightMotor] = power;
+	wait1Msec(msecs);
+}
+void runArmMotors(int power, int msecs){
+	motor[leftArmMotor] = power;
+	motor[rightArmMotor] = power;
+	wait1Msec(msecs);
+}
+//end library
 #endif
